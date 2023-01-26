ffa241e25fafd3bcf0871c6d65e7cd0636ab9559
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-26 07:31:16 +0530

    added missing variable definition authorized_cidr_name

--- terraform/examples/example-terraform-gke-standard-private-cluster/main.tf
+++ terraform/examples/example-terraform-gke-standard-private-cluster/main.tf
@@ -1,6 +1,7 @@
 module "terraform-gke-standard-private-cluster" {
     source = "../../modules/terraform-gke-standard-private-cluster"
     authorized_cidr = var.authorized_cidr
+    authorized_cidr_name = var.authorized_cidr_name
     is_premptible = var.is_premptible
     disk_size_gb = var.disk_size_gb
     node_pool_name = var.node_pool_name
