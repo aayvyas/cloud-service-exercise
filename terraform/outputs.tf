2d55dd205d3716b3947df304d51b27dde6e820b9
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-26 17:39:17 +0530

    removed error causing outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,10 +7,3 @@ output "static_ip_address" {
   value = module.terraform-compute-address.static_ip_address
 }
 
-output "vpc_id" {
-    value = module.terraform-vpc.vpc_id
-}
-
-output "subnet_id" {
-    value = module.terraform-vpc.subnet_id
-}
