7b22871f5195f3ad23be84e27d8f20911c250ff2
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-27 10:04:07 +0530

    changed variables

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,10 +1,14 @@
 # !/bin/bash
 
 echo 'Apache Benchmark Test'
-read 'IP/Url: ' URL
-read 'Number of requests:' n
-read 'Number of concurrent requests': c
+echo 'Enter url:'
+read URL
+echo 'Enter number of requests:'
+read n
+echo 'Enter number of concurrent requests:'
+read c
+
 sudo apt-get -y install apache2-utils 
 sudo apt-get -y install apache2 
 
-ab -$(n) 1000 -$(c) 10 $(URL)
+ab -n $n -c $c $URL
