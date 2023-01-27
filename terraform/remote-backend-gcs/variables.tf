6349f2e38b2526bd7f0adcbeead5987fcd07768c
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-27 15:11:06 +0530

    added provider

--- terraform/remote-backend-gcs/variables.tf
+++ terraform/remote-backend-gcs/variables.tf
@@ -2,3 +2,8 @@ variable "backend_gcs_name" {
     type = string
     description = "google cloud storage backend bucket name (unique)"
 }
+
+variable "project_id" {
+    description = "your project id"
+    type = string
+}
