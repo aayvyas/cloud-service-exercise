d0ea99de761d5df1e68be1541f60cf1669470b6b
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-28 19:35:38 +0530

    added script for provisioning a certificate, added new architecture diagram

---
+++ scripts/tls.sh
@@ -0,0 +1,34 @@
+mkdir ssl-certificate
+openssl genrsa -out ./ssl-certificate/private-key-file 2048
+cat <<'EOF' >./ssl-certificate/config-file
+[req]
+default_bits              = 2048
+req_extensions            = extension_requirements
+distinguished_name        = dn_requirements
+
+
+[extension_requirements]
+basicConstraints          = CA:FALSE
+keyUsage                  = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName            = @sans_list
+
+[dn_requirements]
+countryName               = Country Name (2 letter code)
+stateOrProvinceName       = State or Province Name (full name)
+localityName              = Locality Name (eg, city)
+0.organizationName        = Organization Name (eg, company)
+organizationalUnitName    = Organizational Unit Name (eg, section)
+commonName                = Common Name (e.g. server FQDN or YOUR name)
+emailAddress              = Email Address
+
+[sans_list]
+DNS.1                     = cloudservices.com
+
+EOF
+openssl req -new -key ./ssl-certificate/private-key-file \
+    -out ./ssl-certificate/csr-file \
+    -config ./ssl-certificate/config-file
+cd ./ssl-certificate
+openssl x509 -req     -signkey private-key-file     -in csr-file     -out tls-cert-file     -extfile config-file     -extensions extension_requirements     -days 30
+base64 -w0 tls-cert-file
+base64 -w0 private-key-file
