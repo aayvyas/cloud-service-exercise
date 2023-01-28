edafbdf3ab3b255a294fa70b068a1ed48e0415eb
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-28 07:45:06 +0530

    added default network

--- terraform/examples/example-private-cloud-dns/main.tf
+++ terraform/examples/example-private-cloud-dns/main.tf
@@ -2,7 +2,7 @@ module "terraform-private-cloud-dns" {
 
     source = "../../modules/terraform-private-cloud-dns"
     dns_name = var.dns_name
-    vpc_network = var.vpc_network
+    vpc_network = google_compute_network.default.self_link
     subdomain_name = var.subdomain_name
     ip_address_for_dns = var.ip_address_for_dns
   
