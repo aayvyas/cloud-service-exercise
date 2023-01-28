a775d32712edb9eee21fe9d4a5cba2131b233909
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-28 07:49:19 +0530

    added data source for fetching the default network

--- terraform/examples/example-private-cloud-dns/main.tf
+++ terraform/examples/example-private-cloud-dns/main.tf
@@ -5,7 +5,7 @@ module "terraform-private-cloud-dns" {
 
     source = "../../modules/terraform-private-cloud-dns"
     dns_name = var.dns_name
-    vpc_network = google_compute_network.default.self_link
+    vpc_network = data.google_compute_network.default.self_link
     subdomain_name = var.subdomain_name
     ip_address_for_dns = var.ip_address_for_dns
   
