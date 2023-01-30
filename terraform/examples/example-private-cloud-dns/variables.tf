5a69013cd7143383a0dbc3c2bdd1c1f300ebd67f
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-30 09:59:17 +0530

    removed default values for variables in example

--- terraform/examples/example-private-cloud-dns/variables.tf
+++ terraform/examples/example-private-cloud-dns/variables.tf
@@ -2,7 +2,7 @@ variable "dns_name" {
 
     description = "the domain name you want to use (private) , you don't have to own it"
     type = string
-    default = "cloudservices.com"
+    
 
 }
 
@@ -17,14 +17,14 @@ variable "vpc_network" {
 variable "subdomain_name" {
     description = "subdomain name for your dns"
     type = string 
-    default = "api" 
+    
 }
 
 variable "ip_address_for_dns" {
 
     description = "the ip address to resolve to"
     type = string
-    default = "8.8.8.8"
+    
 
   
 }
