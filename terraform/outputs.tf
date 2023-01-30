a5008cb8d788f146ba93233559689a4822b65db3
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-30 12:47:26 +0530

    modified the ouput variable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "public_endpoint" {
 
-    value = module.terraform-gke-standard-private-cluster.private_endpoint
+    value = module.terraform-gke-standard-private-cluster.public_endpoint
   
 }
 output "static_ip_address" {
