ccb6f5eba0366f283f9339e80a1166d967f29e70
Aayush Vyas <aayvyas@publicisgroupe.net>
2023-01-30 09:01:06 +0530

    changed the cidr

--- terraform/examples/example-terraform-gke-standard-private-cluster/variables.tf
+++ terraform/examples/example-terraform-gke-standard-private-cluster/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "authorized_cidr" {
     type = string  
     description = "a valid CIDR range ex: xxx.xxx.xxx.xxx/xx which will be authorized to talk to controlplane"
-    default = "35.198.197.198/32"
+    default = "35.198.197.198"
 }
 
 variable "authorized_cidr_name" {
